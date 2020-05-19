a25f13ad28348cf1a01c46c94c35df6c672de601
James Hagans <jhagans@acm.org>
2020-05-18 22:16:16 -0400

    add regression

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ https://www.apress.com/us/book/9781484251201
 
 Practical Machine Learning with Rust: Creating Intelligent Applications in Rust
 Bhattacharjee, Joydeep
+
+https://github.com/Apress/practical-machine-learning-w-rust
---
+++ rustymachine-regression/src/main.rs
@@ -0,0 +1,13 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+use std::fs::File;
+use std::vec::Vec;
+use std::error::Error;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+
+fn main() {
+    println!("Hello, world!");
+}
