c86a2f01686e8957defc01bf80b0bc64dbe2d85c
James Hagans <jhagans@acm.org>
2020-07-19 21:44:28 -0400

    update rusty machine regression

--- supervised-learning/src/bin/rustymachine-regression.rs
+++ supervised-learning/src/bin/rustymachine-regression.rs
@@ -1,11 +1,35 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use rusty_machine;
+use rusty_machine::analysis::score::neg_mean_squared_error;
+use rusty_machine::learning::lin_reg::LinRegressor;
+use rusty_machine::learning::SupModel;
+use rusty_machine::linalg::Matrix;
+use rusty_machine::linalg::Vector;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::path::Path;
 use std::vec::Vec;
+
+// for regression
+pub fn r_squared_score(y_test: &[f64], y_preds: &[f64]) -> f64 {
+    let model_variance: f64 = y_test
+        .iter()
+        .zip(y_preds.iter())
+        .fold(0., |v, (y_i, y_i_hat)| v + (y_i - y_i_hat).powi(2));
+
+    // get the mean for the actual values to be used later
+    let y_test_mean = y_test.iter().sum::<f64>() as f64 / y_test.len() as f64;
+
+    // finding the variance
+    let variance = y_test
+        .iter()
+        .fold(0., |v, &x| v + (x - y_test_mean).powi(2));
+    let r2_calculated: f64 = 1.0 - (model_variance / variance);
+    r2_calculated
+}
 
 pub struct BostonHousing {
     // CRIM - per capita crime rate by town
@@ -98,6 +122,64 @@ pub fn get_boston_records_from_file(filename: impl AsRef<Path>) -> Vec<BostonHou
         .collect()
 }
 
-fn main() {
-    println!("Hello, world!");
+pub fn run() -> Result<(), Box<dyn Error>> {
+    // Get all the data
+    let filename = "data/housing.csv";
+    let mut data = get_boston_records_from_file(&filename); // file must be in the folder data
+
+    // shuffle the data.
+    data.shuffle(&mut thread_rng());
+
+    // separate out to train and test datasets.
+    let test_size: f64 = 0.2;
+    let test_size: f64 = data.len() as f64 * test_size;
+    let test_size = test_size.round() as usize;
+    let (test_data, train_data) = data.split_at(test_size);
+    let train_size = train_data.len();
+    let test_size = test_data.len();
+
+    // differentiate the features and the targets.
+    let boston_x_train: Vec<f64> = train_data
+        .iter()
+        .flat_map(|r| r.into_feature_vector())
+        .collect();
+    let boston_y_train: Vec<f64> = train_data.iter().map(|r| r.into_targets()).collect();
+
+    let boston_x_test: Vec<f64> = test_data
+        .iter()
+        .flat_map(|r| r.into_feature_vector())
+        .collect();
+    let boston_y_test: Vec<f64> = test_data.iter().map(|r| r.into_targets()).collect();
+
+    // Convert the data into matrices for rusty machine
+    let boston_x_train = Matrix::new(train_size, 13, boston_x_train);
+    let boston_y_train = Vector::new(boston_y_train);
+    let boston_x_test = Matrix::new(test_size, 13, boston_x_test);
+    // let boston_y_test = Vector::new(boston_y_test);
+    let boston_y_test = Matrix::new(test_size, 1, boston_y_test);
+
+    // Create a linear regression model
+    let mut lin_model = LinRegressor::default();
+    println!("{:?}", lin_model);
+
+    // Train the model
+    lin_model.train(&boston_x_train, &boston_y_train);
+
+    // Now we will predict
+    let predictions = lin_model.predict(&boston_x_test).unwrap();
+    let predictions = Matrix::new(test_size, 1, predictions);
+    let acc = neg_mean_squared_error(&predictions, &boston_y_test);
+    println!("linear regression error: {:?}", acc);
+    println!(
+        "linear regression R2 score: {:?}",
+        r_squared_score(&boston_y_test.data(), &predictions.data())
+    );
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    run()?;
+
+    Ok(())
 }
